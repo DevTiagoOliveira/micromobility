4a5a7aae2b268e1bc33a85e3b8bed0be3dac4bcd
ibbgomes <16123612+ibbgomes@users.noreply.github.com>
2021-12-06 12:41:30 +0000

    Add services config

--- src/services/vehicles/config.js
+++ src/services/vehicles/config.js
@@ -7,5 +7,9 @@ module.exports = Object.freeze({
     server: '127.0.0.1',
     port: 27017,
     name: 'vehicles-service'
+  },
+  pricingService: {
+    server: 'localhost',
+    port: 1005
   }
 })
